bc9b1be4709585f3f6ae689ef900ac95dde71453
Anurag <112074910+Anurag22060@users.noreply.github.com>
2026-06-08 13:35:12 +0530

    cart2

--- addToCartFun/tests/addToCard.spec.js
+++ addToCartFun/tests/addToCard.spec.js
@@ -54,15 +54,15 @@ test('@Webst Client App login', async ({ page }) => {
    const rows = await page.locator("tbody tr");
  
  
-//    for (let i = 0; i < await rows.count(); ++i) {
-//       const rowOrderId = await rows.nth(i).locator("th").textContent();
-//       if (orderId.includes(rowOrderId)) {
-//          await rows.nth(i).locator("button").first().click();
-//          break;
-//       }
-//    }
-//    const orderIdDetails = await page.locator(".col-text").textContent();
-//    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
      
    
 });
---
+++ uniqueLocators/tests/getBy.spec.js
@@ -0,0 +1,8 @@
+import test from '@playwright/test';
+
+test("GetBy Locators", async ({page})=>{
+ await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+
+
+})
